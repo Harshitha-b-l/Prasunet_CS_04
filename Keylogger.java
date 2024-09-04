5352861a01c18c8f4ee05a30175393a5f171d6e8
HARSHITHA B L <harshithadharwad@gmail.com>
2024-09-04 23:26:15 +0530

    Update Keylogger.java

--- Keylogger.java
+++ Keylogger.java
@@ -4,11 +4,14 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Keylogger extends JFrame implements KeyListener {
     
     private static final long serialVersionUID = 1L;
     private BufferedWriter writer;
+    private JLabel label; 
 
     public Keylogger() {
         try {
@@ -16,26 +19,42 @@ public class Keylogger extends JFrame implements KeyListener {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        this.addKeyListener(this);
+        
+        
         this.setTitle("Keylogger");
         this.setSize(400, 200);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        JPanel panel = new JPanel();
+        this.add(panel);
+
+        label = new JLabel("Type something here...");
+        panel.add(label);
+
+        panel.addKeyListener(this);
+        panel.setFocusable(true); 
+        panel.requestFocusInWindow(); 
+
         this.setVisible(true);
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
         logKey(e);
+        System.out.println("Key Typed: " + e.getKeyChar()); 
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        logKey(e);
+        // Optionally log key presses
+        System.out.println("Key Pressed: " + e.getKeyCode()); 
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        logKey(e);
+        // Optionally log key releases
+        System.out.println("Key Released: " + e.getKeyCode()); 
     }
 
     private void logKey(KeyEvent e) {
@@ -44,6 +63,16 @@ public class Keylogger extends JFrame implements KeyListener {
             writer.flush();
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        try {
+            writer.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
